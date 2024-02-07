01bfc6421962ab6e169a405fb422d89389ec7343
Chrislegend27 <batsonfocus@gmail.com>
2024-02-07 16:56:35 -0500

    Update COMPIIPROJ.s

--- COMPIIPROJ.s
+++ COMPIIPROJ.s
@@ -53,6 +53,8 @@ loop:
 	beq $t0, 71, add_16 #G
 	beq $t0, 103, add_16 #g
 	
+	beq $t0, 47, add_16
+	
 	sub $t1, $t0, '0'  # Convert the character to integer and store in $t1 
 	
 	add $t5, $t5, $t1 # Add integer to the Accumulator which is in $t5
@@ -94,6 +96,14 @@ add_15:
 add_16:
 	li $t1, 16
 	add $t5, $t5, $t1
+	j continue_loop
+	
+delimiter:
+	li $v0, 1       # Print the result
+    move $a0, $t5
+    syscall
+	
+	
 	j continue_loop
 	
 continue_loop:
