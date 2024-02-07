a7663e92fe5ba3f3825055031ec4f8ea3416eb8f
Chrislegend27 <batsonfocus@gmail.com>
2024-02-06 20:51:38 -0500

    Update COMPIIPROJ.s

--- COMPIIPROJ.s
+++ COMPIIPROJ.s
@@ -1,16 +1,15 @@
 .data
 newline:        .asciiz "\n"
 input_prompt:   .asciiz "Enter your string: "
-my_string:     .space 1000
+my_string:      .space 1000
 
 .text
 .globl main
 
 main:
-	li $v0, 4       # Print input prompt
+    li $v0, 4       # Print input prompt
     la $a0, input_prompt
     syscall
-	
-done:
-	li $v0, 10      # Exit program
+
+    li $v0, 10      # Exit program
     syscall
